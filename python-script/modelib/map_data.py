c4468c722005a50209f3c37788c8e7cad61c6cda
Ahmad Z. Ihsan <a.z.ihsan@icloud.com>
2022-03-28 13:41:45 +0200

    feature: add nickel dislocation microstructure

--- python-script/modelib/map_data.py
+++ python-script/modelib/map_data.py
@@ -6,15 +6,14 @@ from rdf_serializer import rdf_serializer
 # Load data 
 def main():
     G = Graph()
-    file_path = "../../data/modelib-microstructure/model_ddd_data.h5"
+    file_path = "../../data/modelib-microstructure/modelib-nickel-microstructure.h5"
     data = h5py.File(file_path, "r")
     node_data_50 = data['00000050']['node data']
     linker_data_50 = data['00000050']['linker data']
     loop_data_50 = data['00000050']['loop data']
     IRI = 'http://example.org/dislocation-microstructure/'
-    # IRI = URIRef('http://example.org/dislocation-microstructure')
     G = rdf_serializer(node_data_50, linker_data_50, loop_data_50, IRI)
-    G.serialize(destination='../../data/modelib-microstructure/dislocation-microstructure.ttl', format='turtle')
+    G.serialize(destination='../../data/modelib-microstructure/modelib-nickel-microstructure.ttl', format='turtle')
 
 if __name__ == "__main__":
     main()
